8f360bf98fd6a5b942c9b48e80100e99d2ccecf8
Piotr Wojtas <piotr.wojtas@edu.uekat.pl>
2025-03-16 17:29:23 +0100

    opencv_basic_rotation2

--- z10.py
+++ z10.py
@@ -11,11 +11,12 @@ cv2.imshow("Sauron original", image_s)
 
 angle = 0
 
-for i in range (24):
+for i in range (25):
 
     rotated=imutils.rotate_bound(image_s, angle)
     cv2.imshow(f"Rotated by {angle} Degrees", rotated)
     cv2.waitKey(500)
     angle += 15
 
+cv2.waitKey(0)
 cv2.destroyAllWindows()
